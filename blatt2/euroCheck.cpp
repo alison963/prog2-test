37687edb3f25e2c0406c0f5ef2e0bf4ae113d6d6
Mohammad Ali Mashta <m.mashta@tu-braunschweig.de>
2023-05-10 18:55:11 +0200

    aufgabe 5 fertig, Funktion 2002 richtig implemtiert

--- blatt2/euroCheck.cpp
+++ blatt2/euroCheck.cpp
@@ -1,22 +1,35 @@
 #include "euroCheck.h"
 #include <iostream>
-
+#include <unordered_map>
 using std::string;
 
 bool isLetter(char c);
 bool isNumber(char c);
 int toNumber(char c);
+int neunerrest(int quersumme);
+
+
+unordered_map<char,int> nums2002 {
+        {'Z',9},{'Y',1},{'X',2},{'W',3},{'V',4},{'U',5},
+        {'T',6},{'S',7},{'R',8},{'P',1},{'N',3},{'M',4},
+        {'L',5},{'K',6},{'J',7},{'H',8},{'G',1},
+        {'D',2},{'E',3},{'D',4},{'C',5},{'B',6}
+
+};
 
 // TODO Aufgabe 2:
 //  Implementiert die Funktion `getEuroSerialNumberVersion(serialNumber)`.
 //  Sie soll je nach Version der Banknote das entsprechende Jahr (2013 oder 2002) als `int` zurückgeben, und 0 für ungültige Seriennummern.
 //  Seriennummern vor 2013 haben *einen* Buchstaben am Anfang. Seit 2013 sind es *zwei*.
 int getEuroSerialNumberVersion(string & serialNumber) {
-    if(isLetter(serialNumber[0])) {
-        if (isLetter(serialNumber[1])) {
-            return 2013;
+    if (serialNumber.size() == 12) {
+        if(isLetter(serialNumber[0])) {
+            if (isLetter(serialNumber[1])) {
+                return 2013;
+            }
+            return 2002;
         }
-       return 2002;
+        return 0;
     }
     else return 0;
 }
@@ -27,36 +40,41 @@ bool checkEuroSerialNumber2002(std::string & serialNumber) {
     int numInInt = toNumber(serialNumber[0]);
     string num = to_string(numInInt);
     int quersumme = 0;
-    int ziffer;
-    //addition of the digits from converted letter, could be more than 1 digits number
-    for (int i = 0; i < num.size(); ++i) {
+
+    //quersumme der konvertierten Zahl
+    for (int i = 0; i < num.size() ; ++i) {
         int number = toNumber(num[i]);
         quersumme = quersumme + number;
     }
-    //addition from rest of serialNumber
-    for (int i = 1; i < serialNumber.size()+1; ++i) {
+    //+ die Quersumme über alle Zahlen
+    for (int i = 1; i < serialNumber.size() - 1 ; ++i) {
         int number1 = toNumber(serialNumber[i]);
         quersumme = quersumme + number1;
     }
-
+    int ziffer;
     int number = quersumme % 9;
     if (8 - number== 0 ) {
         ziffer = 0;
     } else ziffer = 8 - number;
-
-    //Überprüfung der Kontrollnumer
-    int quersumme2 = 0;
-    for (int n = 1; n < serialNumber.size()+1; ++n) {
-        int number1 = toNumber(serialNumber[n]);
-        quersumme2 = quersumme2 + number1;
+    int pz = toNumber(serialNumber[11]);
+    if (ziffer == pz) {
+        //Überprüfung der Kontrollnumer
+        int quersumme2 = 0;
+        //die quersumme über der 10 stelligen Nummer
+        for (int n = 1; n < serialNumber.size(); ++n) {
+            int number1 = toNumber(serialNumber[n]);
+            quersumme2 = quersumme2 + number1;
+        }
+        int finalNum = neunerrest(quersumme2);
+        cout << "erster Buchstabe hat den Wert: " << nums2002[serialNumber[0]] <<" und finalwert ist " << finalNum << endl;
+        if(nums2002[serialNumber[0]] == finalNum) {
+            return true;
+        }
+        else
+            return false;
     }
-    quersumme2 = quersumme2 + ziffer;
-    while(quersumme2 > 9) {
-        quersumme2 = quersumme2 / 9;
-    }
-    cout << quersumme2 <<endl;
+    return false;
 
-    return true;
 }
 
 // TODO Aufgabe 3+6:
@@ -86,4 +104,16 @@ int toNumber(char c) {
     }
     std::cout << "Error: toNumber(char): " << c << std::endl;
     return 0;
+}
+
+int neunerrest (int quersumme) {
+    while(quersumme > 9) {
+        string pruefziffer = to_string(quersumme);
+        quersumme = 0;
+        for (int f = 0; f < pruefziffer.size(); ++f) {
+            int nummer = toNumber(pruefziffer[f]);
+            quersumme = quersumme + nummer;
+        }
+    }
+    return quersumme;
 }
--- blatt2/main.cpp
+++ blatt2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <unordered_map>
+#include <map>
 // TODO Aufgabe 3:
 //  Inkludiert die Datei `euroCheck.h`.
 #include "euroCheck.h"
@@ -25,64 +26,33 @@ void checkSerialNumberAndPrintResult(string & serialNumber) {
 
     switch (jahr) {
         case 2013:
-            checkEuroSerialNumber2013(serialNumber);
+            if (checkEuroSerialNumber2013(serialNumber))
+                cout << "gueltig" << endl;
+            else cout << "nicht" << endl;
+
         case 2002:
-            checkEuroSerialNumber2002(serialNumber);
+            if (checkEuroSerialNumber2002(serialNumber))
+                cout << "gueltig" << endl;
+            else cout << "nicht" << endl;
     }
 
 }
 
 int main() {
-    //string serialNumber;
+    string serialNumber;
     // TODO Aufgabe 4:
     //  Nutzt eine Kontrollstruktur, sodass nacheinander beliebig viele Seriennummern geprüft werden können.
 
-//    while (serialNumber != "fertig") {
-//        serialNumber = inputNextSerialNumber();
-//        checkSerialNumberAndPrintResult(serialNumber);
-//    }
-
-//    string serialNumber = "D12345";
-//    checkEuroSerialNumber2002(serialNumber);
-
-    string serialNumber = "N6710165187";
-    int numInInt = toNumber(serialNumber[0]);
-    string num = to_string(numInInt);
-    int quersumme = 0;
-
-    //addition of the digits from converted letter, could be more than 1 digits number
-    for (int i = 0; i < num.size(); ++i) {
-        int number = toNumber(num[i]);
-        quersumme = quersumme + number;
+    while (serialNumber != "fertig") {
+        serialNumber = inputNextSerialNumber();
+        checkSerialNumberAndPrintResult(serialNumber);
     }
-    for (int i = 1; i < serialNumber.size(); ++i) {
-        int number1 = toNumber(serialNumber[i]);
-        quersumme = quersumme + number1;
-    }
-    int ziffer;
-    int number = quersumme % 9;
-    if (8 - number== 0 ) {
-        ziffer = 0;
-    } else ziffer = 8 - number;
-
-    //Überprüfung der Kontrollnumer
-    int quersumme2 = 0;
-    for (int n = 1; n < serialNumber.size(); ++n) {
-        int number1 = toNumber(serialNumber[n]);
-        quersumme2 = quersumme2 + number1;
-    }
-    quersumme2 = quersumme2 + ziffer;
 
 
-    while(quersumme2 > 9) {
-        string pruefziffer = to_string(quersumme2);
-        quersumme2 = 0;
-        for (int f = 0; f < pruefziffer.size(); ++f) {
-            int nummer = toNumber(pruefziffer[f]);
-            quersumme2 = quersumme2 + nummer;
-        }
-    }
-    cout << quersumme2 <<endl;
+
+
+
+
 
         // TODO Aufgabe 1:
         //  Ruft `inputNextSerialNumber()` auf, um eine Seriennummer einzugeben.
