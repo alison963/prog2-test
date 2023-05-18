f0e0ba966e49b7a09d493f61157b550c1e0d2af4
Mohammad Ali Mashta <m.mashta@tu-braunschweig.de>
2023-05-18 19:32:53 +0200

    2

--- blatt3/src/libSea/Object.cpp
+++ blatt3/src/libSea/Object.cpp
@@ -1,5 +1,7 @@
 #include "Object.h"
-
+#include "Coordinates.h"
+#include <iostream>
+using std::cout;
 
 namespace Sea {
 
@@ -10,22 +12,44 @@ namespace Sea {
 
     bool Object::atCoordinates(Coordinates const & coordinates) const
     {
+
         // TODO Aufgabe 2:
         //  Nutzt die Funktionalität von `Coordinates`, um zu bestimmen, ob ein Teil dieses Objekts auf den übergebenen Koordinaten (`coordinates`) liegt.
+        // Bekomme coordinates als parameter, mit den Informationen die x und y des Feldes wo diese cordinates sind
+        // nehme diese und vergleiche sie mit de
+
+        auto orientationAsInt = static_cast<unsigned int> (orientation);
+        if (orientationAsInt == 0) {
+            for (int i = 0; i <= size; ++i) {
+                if (coordinates.samePositionAs(x,y + i)){
+                    cout << "Objekt liegt hier" << std::endl;
+                    return true;
+                }
+            }
+        }
+        else if (orientationAsInt ==1){
+            for (int i = 0; i <= size; ++i) {
+                if (coordinates.samePositionAs(x + i,y)){
+                    cout << "Objekt liegt hier" << std::endl;
+                    return true;
+                }
+            }
+        }
+        cout << "kein Objekt liegt hier" << std::endl;
         return false;
-        /*
-         ???
-         */
+
     }
 
     bool Object::intersectsWith(Object const & otherObject) const
     {
         // TODO Aufgabe 2:
         //  Nutzt die Funktion `atCoordinates(..)`, um zu bestimmen, ob sich dieses und das übergebene Objekt (`otherObject`) überschneiden.
+        Coordinates coo(x,y);
+        if (otherObject.atCoordinates(coo)) {
+            return true;
+        }
         return false;
-        /*
-         ???
-         */
+
     }
 
 }
